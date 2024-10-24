997b8bd4768372439008f5bede01f50c873de4b4
IonutRazvanCopos <130033085+IonutRazvanCopos@users.noreply.github.com>
2024-10-25 01:07:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
 let game = document.getElementById("game");
+let points = 0;
+let gameOver = false;
+
+let score = document.createElement("div");
+score.id = "score";
+score.innerHTML = "Score:"+ ` ${points}`
+game.append(score);
 
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-let plane = {
-    element: document.getElementById("plane"),
-    imgFile: "/plane.png",
-    x: 300,
-    y: 200,
-    width: 100,
-    height: 100
-}
-
-function mouseMoveHandler(e) {
-    const relativeX = e.clientX - game.offsetLeft;
-
-    if (relativeX > 0 && relativeX < game.offsetWidth) {
-        plane.element.style.left = relativeX - 50 + "px";
+    let plane = {
+        element: document.getElementById("plane"),
+        x: 634,
+        y: 930,
+        width: 100,
+        height: 100
     }
-}
+
+    function mouseMoveHandler(e) {
+        const canvasRect = canvas.getBoundingClientRect();
+        const relativeX = e.clientX - canvasRect.left;
+        if (relativeX > 0 && relativeX < canvasRect.width) {
+            plane.x = relativeX;
+            plane.element.style.left = relativeX - plane.width / 2 + "px";
+        }
+    }
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let img = new Image();
+img.src = '/asteroid.png';
 
 let spawnLineY = 0;
-let spawnRate = 1500;
-let spawnRateOfDescent = 3;
+let spawnRate = 1200;
+let spawnRateOfDescent = 8;
 let lastSpawn = 1;
 let objects = [];
 let startTime = Date.now();
@@ -53,7 +62,7 @@ function spawnRandomObject() {
 function animate() {
     let time = Date.now();
 
-    if (time > (lastSpawn + spawnRate)) {
+    if (time > (lastSpawn + spawnRate) && !gameOver) {
         lastSpawn = time;
         spawnRandomObject();
     }
@@ -65,21 +74,59 @@ function animate() {
     ctx.beginPath();
     ctx.moveTo(0, spawnLineY);
     ctx.lineTo(canvas.width, spawnLineY);
-    ctx.stroke();
 
     for (let i = 0; i < objects.length; ++i) {
         let object = objects[i];
         object.y += spawnRateOfDescent;
         ctx.beginPath();
-        ctx.arc(object.x, object.y, 13, 0, Math.PI * 2);
+        ctx.arc(object.x, object.y, 40, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fillStyle = object.type;
         ctx.fill();
-        if (
-            (canvas.height - plane.y + 150 <= object.y)
-        ) {
+        if (isCollide(plane, object)) {
             spawnRateOfDescent = 0;
-            spawnRate *= 100;
         }
+        if (object.y > canvas.height) {
+            object.y = null;
+            updatePoints();
+        }
+    }
+}
+
+function isCollide(airplane, meteorit) {
+    if (
+        ((airplane.y + 10 <= meteorit.y &&
+            airplane.x + 30 >= meteorit.x &&
+            airplane.x - 30 <= meteorit.x) ||
+        ((airplane.y + 50 <= meteorit.y &&
+        airplane.x + 65 >= meteorit.x &&
+        airplane.x - 65 <= meteorit.x))
+        )
+    ) {
+        gameOver = true;
+        updateText();
+        document.removeEventListener("mousemove", mouseMoveHandler, false);
+        return true;
+    }
+    return false;
+}
+
+function updatePoints() {
+    ++points;
+    score.innerHTML = "Score:" + ` ${points}`
+    if (points == 50) {
+        spawnRateOfDescent = 9;
+    } else if (points == 150) {
+        spawnRateOfDescent = 10;
+    }
+}
+
+let txt = document.createElement('div');
+txt.id = 'gameOver';
+
+function updateText() {
+    if (gameOver) {
+        game.append(txt);
+        txt.innerHTML = 'Game Over! Congrats for your score:' + ` ${points}!`;
     }
 }
